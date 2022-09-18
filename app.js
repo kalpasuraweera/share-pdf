b05877265c4f07db5c45d5595150777e98e8ebb6
kalpa madhushan <kalpamadhushan18@gmail.com>
2022-09-18 18:51:17 +0530

    make routes

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-
-
-
-
+const ejs = require('ejs');
+const bodyParser= require('body-parser');
 
 
 const app =express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+"/public"));
+
+
+
+
+
 app.get('/',(req,res)=>{
-	res.send('this is home');
+	res.render('home')
+})
+
+app.get('/upload',(req,res)=>{
+	res.render('upload')
+})
+app.get('/pdf/:pdfId',(req,res)=>{
+	const pdfId= req.params.pdfId;
+	res.render('pdf', {pdf:pdfId})
 })
 
 app.listen(5000, ()=>{
